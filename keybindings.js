980c7a3de1b8cc67fbd37b2f658a151318c2496a
sydney <sydney@hazelight.se>
2020-09-18 13:27:38 +0200

    fix 'navigate to home' binding resulting in focus loss

--- keybindings.js
+++ keybindings.js
@@ -280,6 +280,16 @@ const actionMap =
 	  {
 	    WF.redo();
 	  },
+	  "ctrl-'": t => 
+	  {
+		WF.zoomTo(WF.rootItem());
+		WF.editItemName(WF.rootItem());
+	  },
+	  "ctrl-ä": t => 
+	  {
+		WF.zoomTo(WF.rootItem());
+		WF.editItemName(WF.rootItem());
+	  },
 	  '#': t => 
 	  {
 		SearchWordUnderCursor();
